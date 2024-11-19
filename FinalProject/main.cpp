4e3eec9f1427675fb132a124352a9f4668873093
Chelsea Brown <chelsea.ren.b@gmail.com>
2024-11-18 21:05:33 -0500

    Initial Commit

---
+++ FinalProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FinalProject
+//
+//  Created by Chelsea Brown on 2024-11-18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
